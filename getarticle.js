dee78224acad91b7c7ecf662485ef70fa99f833d
adam-ghafara <adam.ghafara.job@gmail.com>
2023-12-03 12:10:45 +0700

    Fix Index.js

---
+++ getarticle.js
@@ -0,0 +1,7 @@
+const apiUrl = '';
+const getarticle = document.getElementById('getarticle');
+const getTitle = document.getElementById('title');
+const getCategory = document.getElementById('category');
+const getContent = document.getElementById('content');
+const getAuthor = document.getElementById('author');
+const getDate = document.getElementById('date');
---
+++ index.js
@@ -0,0 +1,5 @@
+import {signUp} from './signup.js'
+import {signIn} from './login.js'
+import {signOut} from './signout.js'
+import {postArticle} from './postArticle.js'
+import {getArticle} from './getarticle.js'
